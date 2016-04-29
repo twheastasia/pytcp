52284b1d38a9fcbf9558cb9962c6a00cd7c41a4e
weihai.tang <weihai.tang@alo7.com>
2016-04-29 17:13:49 +0800

    update new file

---
+++ web.py
@@ -0,0 +1,12 @@
+import urllib2
+
+url = "http://www.alo7.com/"
+
+headers = {}
+headers['User-Agent'] = "Googlebot"
+
+request = urllib2.Request(url, headers=headers)
+response = urllib2.urlopen(request)
+
+print response.read()
+response.close()
